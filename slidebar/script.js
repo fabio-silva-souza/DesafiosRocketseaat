d505218fde1ae03a07c02a05d3d0c1492a99e5b3
fabio-silva-souza <silvafabiosouza@hotmail.com>
2023-01-31 21:05:03 -0300

    initial

---
+++ slidebar/script.js
@@ -0,0 +1,7 @@
+function testOpen() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function testClose() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
