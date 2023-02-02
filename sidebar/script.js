ced2258ea29a3f423b92faca3fefc2135a4eb666
fabio-silva-souza <silvafabiosouza@hotmail.com>
2023-02-02 20:15:38 -0300

    remove divs script

--- sidebar/script.js
+++
@@ -1,7 +0,0 @@
-function testOpen() {
-    document.querySelector(".container").style.display = "block";
-  }
-  
-  function testClose() {
-    document.querySelector(".container").style.display = "none";
-  }
